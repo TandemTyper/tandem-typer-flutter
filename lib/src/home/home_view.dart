5e9ab9d2dc8d5bea859fc7795b225647b7b04b1f
Hans Wang <hanswang0168@gmail.com>
2024-05-28 16:18:23 -0400

    feat: crude implementation of wpm

--- lib/src/home/home_view.dart
+++ lib/src/home/home_view.dart
@@ -1,15 +1,17 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:tandem_typer/src/settings/settings_controller.dart';
 import 'package:tandem_typer/src/settings/settings_view.dart';
 
 class HomeView extends StatefulWidget {
-  HomeView({super.key, required this.controller});
+  const HomeView({super.key, required this.controller});
 
   static const routeName = '/';
 
   final SettingsController controller;
+  // static const String _str = 'Lorem ipsum dolor sit amet';
   static const String _str =
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra sem nulla, a sollicitudin dui ultrices in. Morbi lobortis semper arcu, vel porttitor leo pretium sed. Nullam nisl est, facilisis.';
 
@@ -18,39 +20,68 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  int counter = 0;
-  final FocusNode _focusNode = FocusNode();
+  final FocusNode _kbListenerFocusNode = FocusNode();
+  final _inputFieldController = TextEditingController();
 
-  final inputFieldController = TextEditingController();
-  final Iterator<String> iter = HomeView._str.split(' ').iterator;
+  int _numCorrectWordsTyped = 0;
+  Iterator<String> _iter = HomeView._str.split(' ').iterator;
+  bool _typing = false;
+  int _startTime = 0;
+  double _wpm = 0;
+  Timer? _wpmCalculator;
 
-  @override
-  void initState() {
-    iter.moveNext();
-    super.initState();
+  void start() {
+    _typing = true;
+    _startTime = DateTime.now().millisecondsSinceEpoch;
+    _wpmCalculator = Timer.periodic(Duration(seconds: 1), (Timer t) {
+      _wpm = _numCorrectWordsTyped /
+          (DateTime.now().millisecondsSinceEpoch - _startTime) *
+          1000 *
+          60;
+      setState(() {});
+    });
+    setState(() {});
+  }
+
+  void reset() {
+    _inputFieldController.clear();
+    _iter = HomeView._str.split(' ').iterator;
+    _iter.moveNext();
+    _numCorrectWordsTyped = 0;
+    _typing = false;
+    setState(() {});
   }
 
   void onSpacePressed(KeyEvent event) {
-    String word = inputFieldController.text;
+    String word = _inputFieldController.text;
     if (word.isEmpty) return;
-    inputFieldController.clear();
-    if (word == iter.current) {
-      counter++;
+    _inputFieldController.clear();
+    if (word == _iter.current) {
+      _numCorrectWordsTyped++;
     }
-    iter.moveNext();
+    if (!_iter.moveNext()) {
+      reset();
+    }
     setState(() {});
   }
 
   @override
+  void initState() {
+    _iter.moveNext();
+    super.initState();
+  }
+
+  @override
   void dispose() {
-    inputFieldController.dispose();
+    _inputFieldController.dispose();
+    _wpmCalculator?.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return KeyboardListener(
-      focusNode: _focusNode,
+      focusNode: _kbListenerFocusNode,
       autofocus: true,
       onKeyEvent: (event) {
         if (event.logicalKey == LogicalKeyboardKey.space &&
@@ -70,40 +101,60 @@ class _HomeViewState extends State<HomeView> {
             )
           ],
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Center(
-              child: Text('$counter', style: TextStyle(fontSize: 40)),
-            ),
-            Padding(padding: EdgeInsetsDirectional.only(bottom: 100)),
-            const FractionallySizedBox(
-              widthFactor: 0.5,
-              child: Text(
-                HomeView._str,
-                style: TextStyle(fontSize: 30),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('$_numCorrectWordsTyped',
+                      style: const TextStyle(fontSize: 40)),
+                  if (_typing) Text('${_wpm.round()}')
+                ],
               ),
-            ),
-            const Padding(
-                padding: EdgeInsetsDirectional.symmetric(vertical: 8)),
-            FractionallySizedBox(
-              widthFactor: 0.5,
-              child: TextField(
-                controller: inputFieldController,
-                style: const TextStyle(fontSize: 30),
-                decoration: InputDecoration(
-                  hintText: iter.current,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  filled: true,
-                  fillColor: Theme.of(context).hoverColor,
+              const Padding(padding: EdgeInsetsDirectional.only(bottom: 50)),
+              const FractionallySizedBox(
+                widthFactor: 0.5,
+                child: Text(
+                  HomeView._str,
+                  style: TextStyle(fontSize: 30),
                 ),
-                inputFormatters: [FilteringTextInputFormatter.deny(' ')],
               ),
-            ),
-          ],
+              const Padding(
+                  padding: EdgeInsetsDirectional.symmetric(vertical: 8)),
+              FractionallySizedBox(
+                widthFactor: 0.5,
+                child: TextField(
+                  onChanged: (value) {
+                    if (!_typing) {
+                      start();
+                    }
+                  },
+                  controller: _inputFieldController,
+                  style: const TextStyle(fontSize: 30),
+                  decoration: InputDecoration(
+                    hintText: _iter.current,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    filled: true,
+                    fillColor: Theme.of(context).hoverColor,
+                  ),
+                  inputFormatters: [FilteringTextInputFormatter.deny(' ')],
+                ),
+              ),
+              const Padding(padding: EdgeInsetsDirectional.only(bottom: 50)),
+              TextButton(
+                onPressed: () => reset(),
+                child: const Icon(
+                  Icons.refresh,
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
