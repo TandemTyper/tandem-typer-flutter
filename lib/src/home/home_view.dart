353d9a922ee3a4e8ab2ddf53e43458793c9bfff2
Hans Wang <hanswang0168@gmail.com>
2024-05-28 01:56:35 -0400

    feat: add textbox and input field for typing

--- lib/src/home/home_view.dart
+++ lib/src/home/home_view.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:tandem_typer/src/settings/settings_controller.dart';
 import 'package:tandem_typer/src/settings/settings_view.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   HomeView({super.key, required this.controller});
 
   static const routeName = '/';
 
   final SettingsController controller;
+  static const String _str =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra sem nulla, a sollicitudin dui ultrices in. Morbi lobortis semper arcu, vel porttitor leo pretium sed. Nullam nisl est, facilisis.';
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
   final FocusNode _focusNode = FocusNode();
+
+  final inputFieldController = TextEditingController();
+
+  @override
+  void dispose() {
+    inputFieldController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +34,12 @@ class HomeView extends StatelessWidget {
       focusNode: _focusNode,
       autofocus: true,
       onKeyEvent: (event) {
-        print(event);
+        // print(event);
+        if (event.logicalKey == LogicalKeyboardKey.space) {
+          String word = inputFieldController.text;
+          inputFieldController.clear();
+          print(word);
+        }
       },
       child: Scaffold(
         appBar: AppBar(title: const Text("Home"), actions: [
@@ -28,8 +50,36 @@ class HomeView extends StatelessWidget {
             icon: const Icon(Icons.settings),
           )
         ]),
-        body: const Center(
-          child: Text("Hello World!"),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const FractionallySizedBox(
+              widthFactor: 0.5,
+              child: Text(
+                HomeView._str,
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+            const Padding(
+                padding: EdgeInsetsDirectional.symmetric(vertical: 8)),
+            FractionallySizedBox(
+              widthFactor: 0.5,
+              child: TextField(
+                controller: inputFieldController,
+                style: const TextStyle(fontSize: 30),
+                decoration: InputDecoration(
+                  // hintText: 'Type here..',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  filled: true,
+                  fillColor: Theme.of(context).hoverColor,
+                ),
+                inputFormatters: [FilteringTextInputFormatter.deny(' ')],
+              ),
+            ),
+          ],
         ),
       ),
     );
